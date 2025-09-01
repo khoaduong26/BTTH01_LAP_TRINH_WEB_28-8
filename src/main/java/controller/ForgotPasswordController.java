013189dfbe94c6598c8975d5849ed6e940456b1b
khoaduong26 <khoaduong861@gmail.com>
2025-09-01 22:51:57 +0700

    cap nhat bai thuc hanh hoan thien

--- src/main/java/controller/ForgotPasswordController.java
+++ src/main/java/controller/ForgotPasswordController.java
@@ -3,11 +3,15 @@ package controller;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service.UserService;
 import service.*;
-public class ForgotPasswordController {
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/forgot-password")
+public class ForgotPasswordController extends HttpServlet{
 	private UserService userService = new UserServiceImpl();
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
